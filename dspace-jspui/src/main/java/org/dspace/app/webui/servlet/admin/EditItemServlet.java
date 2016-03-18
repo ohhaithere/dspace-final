a46600439e6db9ef84a1e9df3ef419ad1fe934fb
Igor <frostmasterwork@gmail.com>
2016-03-18 09:40:53 -0700

    suggestion

--- dspace-jspui/src/main/java/org/dspace/app/webui/servlet/admin/EditItemServlet.java
+++ dspace-jspui/src/main/java/org/dspace/app/webui/servlet/admin/EditItemServlet.java
@@ -853,7 +853,7 @@ public class EditItemServlet extends DSpaceServlet
         {
             if (itIs == false) {
                 try {
-                    ItemExport.exportItemToFolder(context, item, "/home/dspace/1C", 0, false);
+                    ItemExport.exportItemToFolder(context, item, "/home/dspace/", 0, false);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
